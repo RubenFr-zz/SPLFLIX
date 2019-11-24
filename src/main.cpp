4f0bdc244ecedea2eb3d804df4c82d8ae775a0c3
Ruben Fratty <rubenf@post.bgu.ac.il>
2019-11-24 13:17:22 +0200

    added some tester

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+//    Tester for reader of Json file
 //    const string path = "C:/Users/Ruben/source/repos/RubenFr/SPLFLIX/config2.json";
 //    Session *test = new Session(path);
 //    vector<Watchable*> content = test->getContent();
@@ -16,25 +17,38 @@ int main(int argc, char** argv) {
 //        Watchable* tmp = *it;
 //        cout << tmp->toString();
 //    }
-    CreateUser tmp;
-    string type_str = "";
-    cin >> type_str;
-    Type type = Type::null;
-    if (tmp.getStringToType().count(type_str) > 0) { type = tmp.getStringToType().at(type_str); }
 
-    switch (type)
-    {
-        case(Type::len):
-            cout << "get into len" << endl;
-            break;
-        case(Type::rer):
-            cout << "get into rer" << endl;
-            break;
-        case(Type::gen):
-            cout << "get into gen" << endl;
-            break;
-        default:
-            cout << "error" << endl;
-    }
+// Tester for the switch case with enum
+    CreateUser tmp;
+//    string type_str = "";
+//    cin >> type_str;
+//    Type type = Type::null;
+//    if (tmp.getStringToType().count(type_str) > 0) { type = tmp.getStringToType().at(type_str); }
+//
+//    switch (type)
+//    {
+//        case(Type::len):
+//            cout << "get into len" << endl;
+//            break;
+//        case(Type::rer):
+//            cout << "get into rer" << endl;
+//            break;
+//        case(Type::gen):
+//            cout << "get into gen" << endl;
+//            break;
+//        case(Type::null):
+//            cout << "error !!" << endl;
+//    }
+
+// Tester for function Session::split()
+//    Session tmp;
+//    vector<string> array = tmp.split("Just a small test to check if that works");
+//    for(auto it = array.begin(); it != array.end(); ++it){
+//        cout << *it << endl;
+//    }
+
+// Tester for Session
+    Session *sess = new Session("C:/Users/Ruben/source/repos/RubenFr/SPLFLIX/config2.json");
+    sess->start();
     return 0;
 }
